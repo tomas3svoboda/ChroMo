bf971a102d34671b98deb010dd70345be0b77caa
TomasS <tomas3.svoboda@gmail.com>
2022-07-18 15:13:05 +0200

    testing change

--- functions/Cluster_Exp.py
+++ functions/Cluster_Exp.py
@@ -1,7 +1,7 @@
 def Cluster_Exp(experimentSet):
 
     experimentClusterComp = 1 + experimentSet
-    experimentClusterCompCond = 1 + experimentSet
+    experimentClusterCompCond = 1 + experimentSet + experimentClusterComp
 
     return [experimentClusterComp, experimentClusterCompCond]
 
