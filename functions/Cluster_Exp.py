ebece48655ba519f8fabd3a0a22b7f4d10547f05
Svoboda <svoboda.tomas@siemens.com>
2022-07-18 15:10:49 +0200

    Initial commit

---
+++ functions/Cluster_Exp.py
@@ -0,0 +1,7 @@
+def Cluster_Exp(experimentSet):
+
+    experimentClusterComp = 1 + experimentSet
+    experimentClusterCompCond = 1 + experimentSet
+
+    return [experimentClusterComp, experimentClusterCompCond]
+
