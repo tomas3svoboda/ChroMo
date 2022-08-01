a471a48864192647ddd3d7bcfbd5f83f8cd39b86
Adam Svoboda <svoboad3@fit.cvut.cz>
2022-08-01 14:25:25 +0200

    Mass_Balance_Cor mostly done(needs tweaking), added feedTime to ExperimentCondition

--- functions/Mass_Balance_Cor.py
+++ functions/Mass_Balance_Cor.py
@@ -1,14 +1,27 @@
 from functions.Deep_Copy_ExperimentSet import Deep_Copy_ExperimentSet
 import numpy as np
+import scipy
 
 def Mass_Balance_Cor(experimentSetCor2, experimentSetGauss):
     experimentSetCor3 = Deep_Copy_ExperimentSet(experimentSetCor2)
     for exp2, expG, exp3 in zip(experimentSetCor2.experiments, experimentSetGauss.experiments, experimentSetCor3.experiments):
-        for comp2, compG, comp3 in zip(exp2.experimentComponents, expG.experimentComponents, exp3.experimentComponents):
-            df2 = comp2.concentrationTime
-            dfG = compG.concentrationTime
-            df3 = comp3.concentrationTime
-            # rename to match naming convention to compOutputMass
-            comp_output_mass = np.trapz(y=dfG.iloc[:, 0].to_numpy(), x=dfG.iloc[:, 1].to_numpy())/expG.experimentCondition.flowRate
-            # WORK IN PROGRESS
+        initialFeedTime = exp2.experimentCondition.feedTime
+        #print(initialFeedTime - (initialFeedTime/2), initialFeedTime + (initialFeedTime/2))
+        def Loss_Func(feedTime):
+            outputMassSum = 0.0
+            feedMassSum = 0.0
+            for comp2, compG, comp3 in zip(exp2.experimentComponents, expG.experimentComponents, exp3.experimentComponents):
+                dfG = compG.concentrationTime
+                # rename to match naming convention to compOutputMass and compFeedMass
+                comp_output_mass = np.trapz(y=dfG.iloc[:, 0].to_numpy(), x=dfG.iloc[:, 1].to_numpy())/expG.experimentCondition.flowRate
+                comp_feed_mass = feedTime * comp2.feedConcentration * exp2.experimentCondition.flowRate
+                #print("feedTime * feedConc * flowRate = " + str(feedTime) + " * " + str(comp2.feedConcentration) + " * " + str(exp2.experimentCondition.flowRate) + " = " + str(comp_feed_mass))
+                outputMassSum += comp_output_mass
+                feedMassSum += comp_feed_mass
+                #print("outputMassSum = " + str(outputMassSum))
+                #print("feedMassSum = " + str(feedMassSum))
+            return outputMassSum - feedMassSum
+        newFeedTime = scipy.optimize.minimize_scalar(Loss_Func, bounds=(initialFeedTime - (initialFeedTime/2), initialFeedTime + (initialFeedTime/2)), method='bounded')
+        #print(newFeedTime)
+        exp3.experimentCondition.feedTime = newFeedTime.x
     return experimentSetCor3
--- objects/ExperimentCondition.py
+++ objects/ExperimentCondition.py
@@ -9,7 +9,37 @@ Contains:
 """
 class ExperimentCondition:
     def __init__(self):
-        self.columnDiameter = 0.0
-        self.columnLength = 0.0
-        self.feedVolume = 0.0
-        self.flowRate = 0.0
+        self.columnDiameter = -1.0
+        self.columnLength = -1.0
+        self._feedVolume = -1.0
+        self._feedTime = -1.0
+        self._flowRate = -1.0
+
+    @property
+    def feedTime(self):
+        return self._feedTime
+
+    @property
+    def feedVolume(self):
+        return self._feedVolume
+
+    @property
+    def flowRate(self):
+        return self._flowRate
+
+    @feedTime.setter
+    def feedTime(self, new_val):
+        self._feedTime = new_val
+        self._feedVolume = new_val * self.flowRate
+
+    @feedVolume.setter
+    def feedVolume(self, new_val):
+        self._feedVolume = new_val
+        self._feedTime = new_val/self.flowRate
+
+    @flowRate.setter
+    def flowRate(self, new_val):
+        if self.flowRate != -1:
+            raise Exception("Flow Rate can't be changed!")
+        else:
+            self._flowRate = new_val
