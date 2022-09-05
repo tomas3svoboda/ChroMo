026eb7493b1bdb9967b5835632c6ea69570cb252
Adam Svoboda <svoboad3@fit.cvut.cz>
2022-09-05 12:32:48 +0200

    Added choice of single loss function to Lev2_Loss_Function

--- functions/Lev2_Loss_Function.py
+++ functions/Lev2_Loss_Function.py
@@ -1,13 +1,29 @@
 # Function calculates loss value for level 2 sub-optimization of the main bi-level optimization
 from functions.Single_Loss_Function_Simple import Single_Loss_Function_Simple
+from functions.Single_Loss_Function_Squares import Single_Loss_Function_Squares
+from functions.Single_Loss_Function_LogSimple import Single_Loss_Function_LogSimple
+from functions.Single_Loss_Function_LogSquares import Single_Loss_Function_LogSquares
 import functions.global_ as gl
 
-def Lev2_Loss_Function(params, experimentCluster, porosity):
+"""
+Loss Function options:
+    Default or 'Simple' - Single_Loss_Function_Simple
+    'Squares' - Single_Loss_Function_Squares
+    'LogSimple' - Single_Loss_Function_LogSimple
+    'LogSquares' - Single_Loss_Function_LogSquares
+"""
+def Lev2_Loss_Function(params, experimentCluster, porosity, lossFunction = 'Simple'):
     params2 = [porosity, params[0], params[1]]
-    #print(params2)
+    func = Single_Loss_Function_Simple
+    if lossFunction == 'Squares':
+        func = Single_Loss_Function_Squares
+    elif lossFunction == 'LogSimple':
+        func = Single_Loss_Function_LogSimple
+    elif lossFunction == 'LogSquares':
+        func = Single_Loss_Function_LogSquares
     sum = 0
     for comp in experimentCluster:
-        res = Single_Loss_Function_Simple(params2, comp)
+        res = func(params2, comp)
         sum += res
     #print("lEVEL 2 Loss function value: " + str(sum))
     return sum
