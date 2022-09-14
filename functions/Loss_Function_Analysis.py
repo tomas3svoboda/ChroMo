e8c567a8066ab1a8cdd47611cd985bd0c0f32867
Adam Svoboda <svoboad3@fit.cvut.cz>
2022-09-14 12:25:14 +0200

    Added progress messages to loss function analysis

--- functions/Loss_Function_Analysis.py
+++ functions/Loss_Function_Analysis.py
@@ -29,6 +29,8 @@ def Loss_Function_Analysis(experimentClusterComp,
                 resultArr[x, y] = res
                 y += 1
             x += 1
+            endpoint = xend-((xend-xstart)%xstep)
+            print(str(round((henryConst-xstart) / (endpoint-xstart) * 100)) + "%")
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         X, Y = np.meshgrid(np.arange(ystart, yend, ystep), np.arange(xstart, xend, xstep))
