56f35e2f899ca5cb095b664b5811f3ddb52ebb27
TomasS <tomas3.svoboda@gmail.com>
2023-05-11 14:55:08 +0200

    nonlin slover - changed method to krylov

--- functions/solvers/Nonlin_Solver.py
+++ functions/solvers/Nonlin_Solver.py
@@ -121,7 +121,7 @@ def Nonlin_Solver(flowRate = 500,       # Volume flowrate in [mL/h]
 
         sol = optimize.root(fun=function,
                             x0=c[i - 1, :],
-                            method='hybr',
+                            method='krylov',
                             args=(c[i - 1, :], feed[i], porosity, langmuirConst, saturCoef, disperCoef, flowSpeed))
         c[i, :] = sol.x
         residuals[i] = np.linalg.norm(sol.fun)  # Save the L2-norm of the residuals at each time step
