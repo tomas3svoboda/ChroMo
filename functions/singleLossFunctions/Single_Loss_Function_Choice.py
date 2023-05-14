676d13838468dbe98d852279a3645464598e6a26
Svoboda <adam.svoboda@siemens.com>
2023-05-14 23:27:04 +0200

    added ValueError handling in single loss function choice

--- functions/singleLossFunctions/Single_Loss_Function_Choice.py
+++ functions/singleLossFunctions/Single_Loss_Function_Choice.py
@@ -3,6 +3,7 @@ from functions.singleLossFunctions.Single_Loss_Function_Simple import Single_Los
 from functions.singleLossFunctions.Single_Loss_Function_Squares import Single_Loss_Function_Squares
 from functions.singleLossFunctions.Single_Loss_Function_LogSimple import Single_Loss_Function_LogSimple
 from functions.singleLossFunctions.Single_Loss_Function_LogSquares import Single_Loss_Function_LogSquares
+import math
 
 '''
 Function allowing to choose between loss function based on choice parameter
@@ -14,15 +15,20 @@ Choices:
 Other choices will raise an exception
 '''
 def Single_Loss_Function_Choice(choice, params, experimentComp, solver = 'Lin', factor = 1, spacialDiff = 30, timeDiff = 3000, time = 10800):
-    res = 0
-    if choice == 'Simple':
-        res = Single_Loss_Function_Simple(params, experimentComp, solver, factor, spacialDiff, timeDiff, time)
-    elif choice == 'Squares':
-        res = Single_Loss_Function_Squares(params, experimentComp, solver, factor, spacialDiff, timeDiff, time)
-    elif choice == 'LogSimple':
-        res = Single_Loss_Function_LogSimple(params, experimentComp, solver, factor, spacialDiff, timeDiff, time)
-    elif choice == 'LogSquares':
-        res = Single_Loss_Function_LogSquares(params, experimentComp, solver, factor, spacialDiff, timeDiff, time)
-    else:
-        raise Exception('Unknown Loss function in Single_Loss_Function_Choice')
-    return res
+    try:
+        res = 0
+        if choice == 'Simple':
+            res = Single_Loss_Function_Simple(params, experimentComp, solver, factor, spacialDiff, timeDiff, time)
+        elif choice == 'Squares':
+            res = Single_Loss_Function_Squares(params, experimentComp, solver, factor, spacialDiff, timeDiff, time)
+        elif choice == 'LogSimple':
+            res = Single_Loss_Function_LogSimple(params, experimentComp, solver, factor, spacialDiff, timeDiff, time)
+        elif choice == 'LogSquares':
+            res = Single_Loss_Function_LogSquares(params, experimentComp, solver, factor, spacialDiff, timeDiff, time)
+        else:
+            raise Exception('Unknown Loss function in Single_Loss_Function_Choice')
+        return res
+    except ValueError as e:
+        print("ValueError in single loss function! Returning infinity.")
+        print(e)
+        return math.inf
