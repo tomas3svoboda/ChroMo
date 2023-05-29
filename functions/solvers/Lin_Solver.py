76940c0016cc7160e4ce6a50f293ff17b8659255
TomasS <tomas3.svoboda@gmail.com>
2023-05-29 22:15:37 +0200

    adjustment of denser feed in lin solver. taking care of ratio between feed time and total time in discrete if condition

--- functions/solvers/Lin_Solver.py
+++ functions/solvers/Lin_Solver.py
@@ -8,21 +8,19 @@ import matplotlib.pyplot as plt
 # Numerical solver of the EDM with Linear Isotherm - linear parabolic PDE
 
 def Lin_Solver(flowRate = 50,       # Volume flowrate in [mL/h]
-               length=235,          # Lenght of the packed section in the column [mm]
+               length=235,          # Length of the packed section in the column [mm]
                diameter=16,         # Column diameter [mm]
                feedVol=15,          # Feed injection volume [mL]
                feedConc=150e-3,     # Concentration of the balanced component in the feed [g/mL] or [mg/mm^3]
-               porosity=0.4,        # Total porosity of the sorbent packing [-]
+               porosity=0.4,        # Total porosity of the adsorbent packing [-]
                henryConst=1.9,      # Henry's constant of the linear isotherm [-]
                disperCoef=12,       # Axial dispersion coefficient [mm^2/s]
                Nx = 30,             # Number of spatial differences
-               Nt = 3000,            # Number of time differences
+               Nt = 3000,           # Number of time differences
                time=10800,          # Finite time of the experiment [s]
                debugPrint=False,
                debugGraph=False,
                full=False):
-
-    print(feedVol)
 
     def diagonal_form(a, lower=1, upper=1):
         # Transforms banded matrix into diagonal ordered form
@@ -43,113 +41,113 @@ def Lin_Solver(flowRate = 50,       # Volume flowrate in [mL/h]
         ab = ab[keep_rows, :]
         return ab
 
-    # Calculation of the feed time [s]
+# Calculation of the feed time [s]
     feedTime = (feedVol / flowRate) * 3600
+
     # Calculation of the flow speed [mm/s]
     flowSpeed = (flowRate * 1000/3600) / ((math.pi * (diameter**2) / 4) * porosity)
-    # 1 h = 3600 s
-    # 1 mL = 1000 mm^3
 
-    '''print("flowRate: " + str(flowRate))
-    print("length: " + str(length))
-    print("diameter: " + str(diameter))
-    print("feedVol: " + str(feedVol))
-    print("feedTime: " + str(feedTime))
-    print("feedConc: " + str(feedConc))
-    print("flowSpeed: " + str(flowSpeed))
-    print("porosity: " + str(porosity))
-    print("henryConst: " + str(henryConst))
-    print("disperCoef: " + str(disperCoef))
-    print("Nx: " + str(Nx))
-    print("Nt: " + str(Nt))
-    print("time: " + str(time))'''
+    if debugPrint:
+        print('Flow speed:   ' + str(round(flowSpeed, 2)) + ' [mm/s]')
+        print('Langmuir Constant:   ' + str(round(henryConst, 2)))
+        print('Dispersion Coefficient:   ' + str(round(disperCoef, 2)) + ' mm2/s')
 
-    # Defining constants
-    a = disperCoef/((((1-porosity)*henryConst)/porosity)+1)  # *** !!! PODLE DOKUMENTU
-    b = flowSpeed/((((1-porosity)*henryConst)/porosity)+1)  # *** !!! PODLE DOKUMENTU
-
+    # ________________________________________________________________________
+    # DATA STRUCTURES PREPARATION
 
     x = np.linspace(0, length, Nx)  # Preparation of space vector
     dx = length / Nx  # Calculating space step [mm]
-    t = np.linspace(0, time, Nt)  # Preparation of time vector
-    dt = time / Nt  # Calculating time step [mm]
 
-    '''if dt > feedTime/10:
-        print("WARNING: discretization time step is more than 1/10 of feed time!")'''
+    denseSparseRatio = 0.4  # define ratio between dense and sparse steps
 
-    feedSteps = feedTime // dt  # Whole number of feed iterations
-    feedTimeAprox = feedTime % dt  # approximation of division
+    dense_steps = int(Nt * denseSparseRatio)  # Determine number of dense steps
+    sparse_steps = Nt + 1 - dense_steps  # Determine number of sparse steps
 
-    # Rounding iteration step based on defined feed parameters
-    if feedTimeAprox >= dt/2:
-        feedSteps += 1
+    if feedTime > (time/4):
+        dense_time = feedTime + (feedTime*0.1)
+    elif feedTime > (time/8):
+        dense_time = feedTime + (feedTime*0.2)
+    elif feedTime > (time/40):
+        dense_time = feedTime + (feedTime*2)
+    elif feedTime > (time/80):
+        dense_time = feedTime + (feedTime*4)
+    else:
+        dense_time = feedTime + (feedTime*8)
+
+    # Create time vector with varying step sizes
+    t_dense = np.linspace(0, dense_time, dense_steps)  # Dense grid
+    t_sparse = np.delete(np.linspace(dense_time, time, sparse_steps),0)  # Sparse grid
+    t = np.concatenate((t_dense, t_sparse))  # Combined time vector
+    dt_dense = t_dense[1] - t_dense[0] # Time step size for dense grid
+    dt_sparse = t_sparse[1] - t_sparse[0] # Time step size for sparse grid
+
     # Constructing pulse injection feed vector
-    feed = np.linspace(0, time, Nt)
-    for i in range(0, Nt):
+    feedSteps = int(feedTime // dt_dense)  # Whole number of feed iterations
+    feed = np.zeros(Nt)  # Initialize feed vector
+
+    # Set feed concentration values
+    for i in range(dense_steps):
         if i <= feedSteps:
             feed[i] = feedConc
         else:
             feed[i] = 0
 
-    c = np.zeros((len(t), len(x)))
+    # Preparation of the solution matrix
+    c = np.zeros((Nt, Nx))
 
     C_0 = np.zeros(len(x))  # Implementing initial conditions
     c[0, :] = C_0
 
-    # Crank-Nicolson matrices preparation
-    # A.c(t+1) = B.c(t)x, where c(t+1) and c(t) are vectors of c(x) values
-    # Preparation of boundaries in matrix A
-    A = np.zeros((Nx, Nx))  # A matrix data structure
-    A[0, 0] = flowSpeed + 1/(2*dx)  # Left boundary
-    A[0, 1] = -1/(2*dx)  # Left boundary
-    A[Nx - 1, Nx - 2] = -1/(2*dx)  # Right boundary
-    A[Nx - 1, Nx - 1] = 1/(2*dx)  # Right Boundary
-
-    # Preparation of boundaries in matrix B
-    B = np.zeros((Nx, Nx))  # B matrix data structure
-    B[0, 0] = -1/(2*dx)  # Left boundary
-    B[0, 1] = 1/(2*dx)  # Left boundary
-    B[Nx - 1, Nx - 2] = 1/(2*dx)  # Right boundary
-    B[Nx - 1, Nx - 1] = -1/(2*dx)  # Right Boundary
-
-    # Filling up Matrices A and B
-    for i in range(1, Nx - 1):
-        A[i, i - 1] = -((dt*a)/(2*(dx**2)))-((dt*b)/(4*dx))
-        A[i, i] = 1 + ((dt*a)/(dx**2))
-        A[i, i + 1] = -((dt*a)/(2*(dx**2)))+((dt*b)/(4*dx))
-        B[i, i - 1] = ((dt*a)/(2*(dx**2)))+((dt*b)/(4*dx))
-        B[i, i] = 1 - ((dt*a)/(dx**2))
-        B[i, i + 1] = ((dt*a)/(2*(dx**2)))-((dt*b)/(4*dx))
-    A_diag = diagonal_form(A)
-
-    Aabs = np.abs(A)
-    if debugPrint:
-        for i in range(0, Nx):
-            if Aabs[i, i] <= np.sum(Aabs[i, :]) - Aabs[i, i]:
-                print('Matrix A is not strictly diagonally dominant in row ' +
-                      str(i) + 'therefore, iterative method may not coverge.\n')
-                break
-            elif i == Nx - 1:
-                print('Matrix A is strictly diagonally dominant,' +
-                      'therefore, iterative method will converge!\n')
-
     # Implementing discretization
     for i in range(1, Nt):  # Advance in time
-        b = B.dot(c[i - 1, :])
-        b[0] = b[0] + flowSpeed * feed[i]  # From left boundary (start at 1 ???)
-        c[i, :] = linalg.solve_banded((1, 1), A_diag, b)
-        # c[i,:] = linalg.solve(A, b) # Solve linear system of algebraic equations
         if debugPrint:
             if i == 1:
                 print('Solution algorithm has been started:')
             if i % (Nt // 20) == 0:
                 print(str(i) + ' steps has been finished ... ' +
                       str(Nt - i) + ' steps remain.')
+
+        if i <= dense_steps:
+            dt = dt_dense
+        else:
+            dt = dt_sparse
+
+        # Crank-Nicolson matrices
+        # A.c(t+1) = B.c(t)x, where c(t+1) and c(t) are vectors of c(x) values
+        # boundaries in matrix A
+        A = np.zeros((Nx, Nx))  # A matrix data structure
+        A[0, 0] = flowSpeed + 1/(2*dx)  # Left boundary
+        A[0, 1] = -1/(2*dx)  # Left boundary
+        A[Nx - 1, Nx - 2] = -1/(2*dx)  # Right boundary
+        A[Nx - 1, Nx - 1] = 1/(2*dx)  # Right Boundary
+        # boundaries in matrix B
+        B = np.zeros((Nx, Nx))  # B matrix data structure
+        B[0, 0] = -1/(2*dx)  # Left boundary
+        B[0, 1] = 1/(2*dx)  # Left boundary
+        B[Nx - 1, Nx - 2] = 1/(2*dx)  # Right boundary
+        B[Nx - 1, Nx - 1] = -1/(2*dx)  # Right Boundary
+        # Defining constants
+        a = disperCoef/((((1-porosity)*henryConst)/porosity)+1)
+        b = flowSpeed/((((1-porosity)*henryConst)/porosity)+1)
+        # Filling up Matrices A and B
+        for j in range(1, Nx - 1):
+            A[j, j - 1] = -((dt*a)/(2*(dx**2)))-((dt*b)/(4*dx))
+            A[j, j] = 1 + ((dt*a)/(dx**2))
+            A[j, j + 1] = -((dt*a)/(2*(dx**2)))+((dt*b)/(4*dx))
+            B[j, j - 1] = ((dt*a)/(2*(dx**2)))+((dt*b)/(4*dx))
+            B[j, j] = 1 - ((dt*a)/(dx**2))
+            B[j, j + 1] = ((dt*a)/(2*(dx**2)))-((dt*b)/(4*dx))
+        A_diag = diagonal_form(A)
+
+        b = B.dot(c[i - 1, :])
+        b[0] = b[0] + flowSpeed * feed[i]  # From left boundary (start at 1 ???)
+        c[i, :] = linalg.solve_banded((1, 1), A_diag, b)
+
     if debugPrint:
         feedMass = feedVol * feedConc  # Calculating theoretical mass fed into system
-        massCumulOut = 0  # Mass cumulation over time in outlet from the column
+        massCumulOut = 0  # Mass accumulation over time in outlet from the column
 
-        for i in range(0, Nt):  # Calculation of mass cumulation over time
+        for i in range(0, Nt):  # Calculation of mass accumulation over time
             actConcOut = c[i, -1]
             massCumulOut += (dt * flowRate * actConcOut / 3600)
 
